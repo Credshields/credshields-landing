6a5960da4df47d32eac2626e2a07246650ffa8f1
FlameCloud <89596368+flamecloudcouk@users.noreply.github.com>
2021-10-28 00:44:12 +0530

    Added files

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(function(){
+	/* all slider start */
+    $('.company_sliders').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true,
+    });
+});
